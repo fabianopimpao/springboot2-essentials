f39f71fbc1746a7e5f6b8c5e332d0dd04fe67dab
fabianopimpao <fabiano.pimpao@gmail.com>
2021-04-21 15:46:42 -0300

    The get method that returns a anime was created

---
+++ training-07/springboot2-anime/src/main/java/me/pimpao/springboot2/service/AnimeService.java
@@ -0,0 +1,24 @@
+package me.pimpao.springboot2.service;
+
+import me.pimpao.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+    List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Samurai X"), new Anime(3L, "Naruto"));
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+}
