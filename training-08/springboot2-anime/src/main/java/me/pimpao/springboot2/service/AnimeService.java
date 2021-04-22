ce94021b53b30b9bf56abc2b143758095d6f5eb8
fabianopimpao <fabiano.pimpao@gmail.com>
2021-04-22 08:00:55 -0300

    The post method added to AnimeControlle class

---
+++ training-08/springboot2-anime/src/main/java/me/pimpao/springboot2/service/AnimeService.java
@@ -0,0 +1,36 @@
+package me.pimpao.springboot2.service;
+
+import me.pimpao.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class AnimeService {
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Samurai X"), new Anime(3L, "Naruto")));
+    }
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(4, 1000000));
+        animes.add(anime);
+        return anime;
+    }
+}
