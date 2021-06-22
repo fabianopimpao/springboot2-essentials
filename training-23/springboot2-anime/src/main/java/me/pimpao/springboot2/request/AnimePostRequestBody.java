f73b8a40235c1ef9983e3098b5cca554fe5124f5
fabianopimpao <fabiano.pimpao@gmail.com>
2021-06-21 23:16:50 -0300

    Global configuration with WebMvcConfigurer to pagination

---
+++ training-23/springboot2-anime/src/main/java/me/pimpao/springboot2/request/AnimePostRequestBody.java
@@ -0,0 +1,11 @@
+package me.pimpao.springboot2.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class AnimePostRequestBody {
+    @NotEmpty(message = "The anime name cannot be empty")
+    private String name;
+}
