c6fdeb276cb29f0f2d82a948ed8085d83b03e73c
fabianopimpao <fabiano.pimpao@gmail.com>
2023-03-30 08:15:44 -0300

    Hot swap with SpringBoot devtools

---
+++ training-04/springboot2-anime/src/main/java/me/pimpao/springboot2/controller/AnimeController.java
@@ -0,0 +1,26 @@
+package me.pimpao.springboot2.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import me.pimpao.springboot2.domain.Anime;
+import me.pimpao.springboot2.util.DateUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+    private final DateUtil dateUtil;
+
+    @GetMapping(path = "list")
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Samurai X"));
+    }
+}
