5ec2f7d4f482c4badf5a6e2e6404f3fb7e1211da
fabianopimpao <fabiano.pimpao@gmail.com>
2023-04-03 14:56:42 -0300

    Put http method created

---
+++ training-10/springboot2-anime/src/test/java/me/pimpao/springboot2/Springboot2AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pimpao.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2AnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
