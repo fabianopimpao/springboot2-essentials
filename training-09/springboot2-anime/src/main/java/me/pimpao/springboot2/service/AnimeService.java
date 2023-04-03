0b14d7b621a108331e8848c9b7116be7d8109909
fabianopimpao <fabiano.pimpao@gmail.com>
2023-04-03 08:28:10 -0300

    Delete http method created

---
+++ training-09/springboot2-anime/src/main/java/me/pimpao/springboot2/service/AnimeService.java
@@ -0,0 +1,40 @@
+package me.pimpao.springboot2.service;
+
+import me.pimpao.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class AnimeService {
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Samurai X")));
+    }
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(Long id) {
+        animes.remove(findById(id));
+    }
+}
